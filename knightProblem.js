1460da606ebc74c040494af9830a82a1c698fae6
Zaid Khan <53516135+kzaidd@users.noreply.github.com>
2023-01-11 09:56:57 +0530

    Update knightProblem.js

--- knightProblem.js
+++ knightProblem.js
@@ -14,11 +14,9 @@ function chessKnight(cell) {
         return (cellPosition > 0 && cellPosition < 9);
     })
     
-    //We now have 2 seperate arrays: One for X Positions, One for Y Positions.
+    //We have 2 seperate arrays: One for X Positions, One for Y Positions.
     //Go thru every combination possible and if it is a valid position then push it
     //to the possible coordinates array - if not present already.
-    //The trick is to validate the position pair (X, Y) by making sure that 
-    //the net X distance plus net Y distance is 3
     for (var i = 0; i < cellXpositions.length; i++) {
         for (var j = 0; j < cellYpositions.length; j++) {
             if (Math.abs(cellX - cellXpositions[i]) + Math.abs(cellY - cellYpositions[j]) === 3) {
