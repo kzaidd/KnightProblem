d5ffc69016b8a0cb052e652637726a24dd329bb3
Zaid Khan <53516135+kzaidd@users.noreply.github.com>
2023-01-11 09:56:15 +0530

    Update knightProblem.js

--- knightProblem.js
+++ knightProblem.js
@@ -1,16 +1,16 @@
 function chessKnight(cell) {
-    var possibleCoordinates = [];
-    var xCoordinates = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
-    var cellX = xCoordinates.indexOf(cell[0]) + 1; //The X Position
-    var cellY = parseInt(cell[1]); //The Y Position
+    let possibleCoordinates = [];
+    let xCoordinates = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
+    let cellX = xCoordinates.indexOf(cell[0]) + 1; //The X Position
+    let cellY = parseInt(cell[1]); //The Y Position
     
     //Find all possible X Positions
-    var cellXpositions = [cellX + 2, cellX - 2, cellX + 1, cellX - 1].filter(function(cellPosition) {
+    let cellXpositions = [cellX + 2, cellX - 2, cellX + 1, cellX - 1].filter(function(cellPosition) {
         return (cellPosition > 0 && cellPosition < 9);
     })
     
     //Find all possible Y Positions
-    var cellYpositions = [cellY + 2, cellY - 2, cellY + 1, cellY - 1].filter(function(cellPosition) {
+    let cellYpositions = [cellY + 2, cellY - 2, cellY + 1, cellY - 1].filter(function(cellPosition) {
         return (cellPosition > 0 && cellPosition < 9);
     })
     
